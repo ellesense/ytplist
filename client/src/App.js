5c3dc7fcfdb6ed657517c146329f53a881df400e
Steve Kim <dkim.steve@gmail.com>
2020-12-17 19:18:45 -0800

    redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,30 @@ import Menubar from "./components/Menubar";
 import LandingPage from "./components/LandingPage";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import { Provider } from "react-redux";
+import reduxStore from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Menubar />
-      <div className="ui container">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return <Redirect to="/home" />;
-            }}
-          />
-          <Route exact path="/home" component={LandingPage} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Provider store={reduxStore}>
+      <BrowserRouter>
+        <Menubar />
+        <div className="ui container">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return <Redirect to="/home" />;
+              }}
+            />
+            <Route exact path="/home" component={LandingPage} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
